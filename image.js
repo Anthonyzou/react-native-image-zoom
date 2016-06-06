059c04941266a73d62a2f53fe0597f30a05bc171
Arthur.Whtoo <whtoo@qq.com>
2016-06-06 15:43:20 +0800

    resolve complie error

--- image.js
+++ image.js
@@ -1,12 +1,3 @@
-import { Platform } from 'react-native'
-//Please declare your exports in this way,if
-//it depends on Platform
-if (Platform.OS == 'ios') {
-  var ZoomImage = require('./index.ios.js')
-  module.exports = ZoomImage
+import ZoomImage from './index'
 
-}
-else{
-  var ZoomImage = require('./index.android.js')
-  module.exports = ZoomImage
-}
+export default ZoomImage
